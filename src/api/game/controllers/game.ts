708493e8827938c700b4f8a7d5304f673f104ad9
vagnealves <vagnerdw1@gmail.com>
2023-10-19 21:18:38 -0300

    Create populate Service

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -3,6 +3,7 @@ import { factories } from '@strapi/strapi'
 export default factories.createCoreController('api::game.game', ({strapi})=>({
     async populate(ctx) {
         console.log("rodando no servidor");
+        await strapi.service("api::game.game").populate(ctx.query)
         ctx.send("finalizado no client")
     }
 }));
